a769d2cd8354077214cc1fc6f67c86557c003f64
giasbd67-tech <giasbd67@gmail.com>
2026-04-08 19:56:02 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,18 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF',
+      },
+      fontFamily: {
+        bengali: ['Hind Siliguri', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
